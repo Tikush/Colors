d41ff1a920d08528594ff0a030461ec82e259426
Tikush <tikush23@gmail.com>
2022-11-16 18:33:11 +0400

    Project of Color
    add full project

---
+++ TikoJanikashvili-Lecture12/ColorViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ColorViewController.swift
+//  TikoJanikashvili-Lecture12
+//
+//  Created by Tiko Janikashvili on 16.11.22.
+//
+
+import UIKit
+
+protocol ColorViewControllerDelegate {
+    func changeButton(color: UIColor?)
+    func changeView(color: UIColor?)
+}
+
+class ColorViewController: UIViewController {
+    
+    // MARK: - IBOutlets
+    
+    @IBOutlet private weak var contentView: UIView!
+    
+    // MARK: - Properties
+    
+    var delegate: ColorViewControllerDelegate?
+
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+    }
+    
+    
+    // MARK: - Change corner of view
+    
+    private func setup() {
+        contentView.layer.cornerRadius = 10
+    }
+    
+    // MARK: - IBActions
+    
+    @IBAction func changeButtonColor(_ sender: UIButton) {
+        delegate?.changeButton(color: sender.backgroundColor)
+        self.dismiss(animated: true)
+    }
+    
+    @IBAction func changeViewColor(_ sender: UIButton) {
+        delegate?.changeView(color: sender.backgroundColor)
+        self.dismiss(animated: true)
+    }
+}
+
---
+++ TikoJanikashvili-Lecture12/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  TikoJanikashvili-Lecture12
+//
+//  Created by Tiko Janikashvili on 16.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK: - IBOutlets
+    
+    @IBOutlet private weak var contentView: UIView!
+    @IBOutlet private weak var colorButton: UIButton!
+    
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+    }
+    
+    // MARK: - Set corner of button
+    
+    private func setup() {
+        colorButton.layer.cornerRadius = 10
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ColorViewController" {
+            if let vc = segue.destination as? ColorViewController {
+                vc.delegate = self
+            }
+        }
+    }
+}
+
+// MARK: - Extension
+
+extension ViewController: ColorViewControllerDelegate {
+    func changeButton(color: UIColor?) {
+        colorButton.backgroundColor = color
+    }
+    
+    func changeView(color: UIColor?) {
+        contentView.backgroundColor = color
+    }
+}
+
